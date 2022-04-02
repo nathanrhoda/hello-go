1db0c87d4ea9f5045fbfb5bce64028a13255e8d5
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-04-02 21:00:33 +0200

    var declaration

--- varPackage/variables.go
+++ varPackage/variables.go
@@ -3,6 +3,16 @@ package varPackage
 import "fmt"
 
 func Woof() {
-	name := "Woof"
-	fmt.Println(name)
+
+	var n string = "Boom"
+	fmt.Println("String variable: " + n)
+
+	var i int = 100
+	fmt.Println(i)
+
+	var f float64 = 0.5
+	fmt.Println(f)
+
+	var b bool = false
+	fmt.Println(b)
 }
