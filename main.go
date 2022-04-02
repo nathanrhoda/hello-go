dfbc68d393ea2bdaa92b533ed20452d8302642b5
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-04-02 20:50:36 +0200

    Moved variable decl to its own file

--- main.go
+++ main.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 	"main/testPackage"
 	"main/testPackage2"
+	"main/varPackage"
 )
 
 func main() {
 	fmt.Println("Hello world")
 	fmt.Println(testPackage.Greet())
 	testPackage2.Mew()
-
-	name := "String variable"
-
-	fmt.Println(name)
+	varPackage.Woof()
 }
---
+++ varPackage/variables.go
@@ -0,0 +1,8 @@
+package varPackage
+
+import "fmt"
+
+func Woof() {
+	name := "Woof"
+	fmt.Println(name)
+}
