e7d0f50b56437ab9c40b781b9874b1c3d33ac9e2
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-04-02 20:20:34 +0200

    Basic syntax

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hello-go
 Into into the go programming language
+
+
+## Geting Started
+
+1) go mod init <Module Name>
+2) go run .
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"main/testPackage"
+	"main/testPackage2"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	fmt.Println(testPackage.Greet())
+	testPackage2.Mew()
+}
