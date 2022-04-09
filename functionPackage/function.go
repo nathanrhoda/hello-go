d16d34f4b5815d1be5236c5a35547c6f5aa2ff54
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-04-09 06:25:46 +0200

    Varying params count

--- functionPackage/function.go
+++ functionPackage/function.go
@@ -8,7 +8,8 @@ func Function() {
 	fmt.Println(addNumbers(1, 2))
 	fmt.Println(multiReturn())
 	fmt.Println(multiReturnWithName())
-	fmt.Println(Veriadic("loop", 1, 2, 3, 4))
+	fmt.Println(Variadic("loop", 1, 2, 3, 4))
+	fmt.Println(Variadic("Variadic", 1, 2))
 }
 
 func addNumbers(a int, b int) int {
@@ -26,6 +27,6 @@ func multiReturnWithName() (num int, bark string) {
 	return num, bark
 }
 
-func Veriadic(looper string, numbers ...int) (string, []int) {
+func Variadic(looper string, numbers ...int) (string, []int) {
 	return looper, numbers
 }
