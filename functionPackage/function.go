9d5d8a98e30bb4955dafbc7f69c7fc644ca85807
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-04-09 06:21:10 +0200

    multi return with name

--- functionPackage/function.go
+++ functionPackage/function.go
@@ -7,6 +7,7 @@ import (
 func Function() {
 	fmt.Println(addNumbers(1, 2))
 	fmt.Println(multiReturn())
+	fmt.Println(multiReturnWithName())
 }
 
 func addNumbers(a int, b int) int {
@@ -16,4 +17,10 @@ func addNumbers(a int, b int) int {
 
 func multiReturn() (int, string) {
 	return 1, "woof"
+}
+
+func multiReturnWithName() (num int, bark string) {
+	bark = "meow"
+	num = 2
+	return num, bark
 }
