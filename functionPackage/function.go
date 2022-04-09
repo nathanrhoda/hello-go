0c1e845e432e5812c0baa7f252c1dac693da2c92
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-04-09 06:18:15 +0200

    muilit return from function

--- functionPackage/function.go
+++ functionPackage/function.go
@@ -6,9 +6,14 @@ import (
 
 func Function() {
 	fmt.Println(addNumbers(1, 2))
+	fmt.Println(multiReturn())
 }
 
 func addNumbers(a int, b int) int {
 	sum := a + b
 	return sum
+}
+
+func multiReturn() (int, string) {
+	return 1, "woof"
 }
