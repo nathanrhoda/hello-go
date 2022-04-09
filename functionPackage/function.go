c6c1a4444e0516a209d1d94af59246e361eff02e
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-04-09 06:24:58 +0200

    Veriadic functions

--- functionPackage/function.go
+++ functionPackage/function.go
@@ -8,6 +8,7 @@ func Function() {
 	fmt.Println(addNumbers(1, 2))
 	fmt.Println(multiReturn())
 	fmt.Println(multiReturnWithName())
+	fmt.Println(Veriadic("loop", 1, 2, 3, 4))
 }
 
 func addNumbers(a int, b int) int {
@@ -23,4 +24,8 @@ func multiReturnWithName() (num int, bark string) {
 	bark = "meow"
 	num = 2
 	return num, bark
+}
+
+func Veriadic(looper string, numbers ...int) (string, []int) {
+	return looper, numbers
 }
