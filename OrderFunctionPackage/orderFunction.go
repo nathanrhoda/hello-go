91d11e3378176fece1b7696972e03230e801f779
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-04-10 19:05:20 +0200

    High Order function refactoring

--- OrderFunctionPackage/orderFunction.go
+++ OrderFunctionPackage/orderFunction.go
@@ -3,9 +3,8 @@ package orderFunctionPackage
 import "fmt"
 
 func OrderFunctions() {
-	Before()
-	println(HighOrder())
-	println(LowOrder())
+	//Before()
+	After()
 }
 
 func Before() {
@@ -28,15 +27,36 @@ func Before() {
 	}
 }
 
-func HighOrder() string {
+//High order function
+func After() {
+	var query int
+	var radius float64
 
-	return "High"
+	fmt.Println("Enter the radius of the circle")
+	fmt.Scanf("%f\n", &radius)
+	fmt.Printf("Enter \n 1 - area \n 2 - perimeter \n 3 - diameter: ")
+	fmt.Scanf("%d", &query)
+
+	printResult(radius, getFunction(query))
 }
 
-func LowOrder() string {
-	return "Low"
+func printResult(radius float64, calcFunction func(r float64) float64) {
+	result := calcFunction(radius)
+	fmt.Println("Result: ", result)
+	fmt.Println("Thank you!")
 }
 
+func getFunction(query int) func(r float64) float64 {
+	query_to_func := map[int]func(r float64) float64{
+		1: calcArea,
+		2: calcPerimeter,
+		3: calcDiameter,
+	}
+
+	return query_to_func[query]
+}
+
+// Method for calculations
 func calcArea(r float64) float64 {
 	return 3.14 * r * r
 }
